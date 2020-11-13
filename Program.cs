a85755555b87047f4a5fddaeeb739e18bc1ddc80
André Luján Longoria <andrelujlon@gmail.com>
2020-11-12 20:35:06 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Manejador_de_memoria_virtual__Simulador_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto Final Sistemas Operativos");
+            Console.ReadKey(); //Espera a que apretemos una tecla -----Para que el resultado salga claro---- 
+        }
+    }
+}
