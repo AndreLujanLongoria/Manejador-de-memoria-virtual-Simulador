664d3335f2960409197a5131ffda81db1b746bdd
Rodrigo Bilbao <rbilbao98@gmail.com>
2020-11-25 15:35:28 -0600

    Leer archivo

---
+++ Globales.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Manejador_de_memoria_virtual__Simulador_
+{
+    public static class Globales
+    {
+        public static List<string> comandos = new List<string>();
+        public static int[] memoriaReal = new int[2048];
+        public static int[] memoriaSwapping = new int[4096];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,52 @@
 ﻿using System;
+using System.IO;
 
 namespace Manejador_de_memoria_virtual__Simulador_
 {
     class Program
-    {
-
-        int counter = 0;
-
+    {        
         static void Main(string[] args)
         {
-            Console.WriteLine("Proyecto Final Sistemas Operativos");
-            Console.ReadKey(); //Espera a que apretemos una tecla -----Para que el resultado salga claro---- 
+            obtenerComandos();
+            procesar();
+
+            Console.ReadKey(); //Espera a que apretemos una tecla -----Para que el resultado salga claro----
+        }
+
+        static void procesar()
+        {
+
+        }
+
+        static void obtenerComandos()
+        {
+            try
+            {
+                // Obtener el archivo txt en base al folder relativo donde se ejecuto el programa
+                // Utilizar al final cuando se tenga que obtener archivo txt relativo al exe
+                //Console.WriteLine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase));
+
+                // Obtener lista de comandos del archivo txt
+                
+                StreamReader archivo = new StreamReader(@"c:/../../../../ArchivoTrabajo.txt");
+
+                if (archivo.Peek() == -1)
+                {
+                    throw new Exception(); // Archivo vacio (invalido)
+                }
+
+                string linea;
+                while ((linea = archivo.ReadLine()) != null)
+                {
+                    Globales.comandos.Add(linea.Trim());
+                }
+
+                archivo.Close();
+            }
+            catch // Error cargando archivo
+            {
+                Console.WriteLine("ERROR: El archivo esta vacio o no existe.");
+            }
         }
     }
 }
